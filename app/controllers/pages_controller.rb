d6aa8ae86436014208321db9d0f30dcb27788954
Jakub Jencik <jakub.jencik@gmail.com>
2017-03-02 20:03:22 +0000

    added bootstrap navbar, created basic pages structure, enabled javascript for bootstrap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,21 @@ class PagesController < ApplicationController
     def home
     end
     
+    def kitchen
+    end
+    
+    def gen_settings
+    end
+    
+    def privacy
+    end
+    
+    def terms_conditions
+    end
+    
+    def contact_us
+    end
+    
     def about
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'kitchen', to: 'pages#kitchen'
+  get 'gen_settings', to: 'pages#gen_settings'
+  get 'privacy', to: 'pages#privacy'
+  get 'terms_conditions', to: 'pages#terms_conditions'
+  get 'contact_us', to: 'pages#contact_us'
   get 'about', to: 'pages#about'
 end

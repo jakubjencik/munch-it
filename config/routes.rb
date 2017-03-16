dddf78e65d4485db5494573ab7099e9762e43421
Jakub Jencik <jakub.jencik@gmail.com>
2017-03-16 00:53:29 +0000

    added contact us form, created controller, route, views file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'gen_settings', to: 'pages#gen_settings'
   get 'privacy', to: 'pages#privacy'
   get 'terms_conditions', to: 'pages#terms_conditions'
-  get 'contact_us', to: 'pages#contact_us'
+  resources :contacts
   get 'about', to: 'pages#about'
+  
 end

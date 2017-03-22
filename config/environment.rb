b858f21843ab02f4f86b1c6802009961d32749b2
Jakub Jencik <jakub.jencik@gmail.com>
2017-03-22 18:51:54 +0000

    Corrected Typos

--- config/environment.rb
+++ config/environment.rb
@@ -12,4 +12,4 @@ ActionMailer::Base.smtp_settings = {
     :domain => 'http://fierce-woodland-32894.herokuapp.com/',
     :authentication => :plain,
 }
-ActionMailer::Base.deliver_method = :smtp
+ActionMailer::Base.delivery_method = :smtp

11f55148b449d53ea6a6d236af0752e452b94dda
Jakub Jencik <jakub.jencik@gmail.com>
2017-03-22 19:25:25 +0000

    environment.rb changed domain to https://...

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
     :address =>ENV['MAILGUN_SMTP_SERVER'],
     :user_name =>ENV['MAILGUN_SMTP_LOGIN'],
     :password =>['MAILGUN_SMTP_PASSWORD'],
-    :domain => 'http://fierce-woodland-32894.herokuapp.com/',
+    :domain => 'https://fierce-woodland-32894.herokuapp.com/',
     :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp

c06d115497551e36da669549a6a0dd18f60be513
Jakub Jencik <jakub.jencik@gmail.com>
2017-03-22 18:54:19 +0000

    moved config code within application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,8 @@ module Saasapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # Required for Heroku in order to show images in assets pipeline
-    config.serve_static_assets = true
   end
 end
 
-
+# Required for Heroku in order to show images in assets pipeline
+config.serve_static_assets = true
